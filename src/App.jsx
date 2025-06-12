08b56afb68c890577f97797c3a828b98e30e273e
RohanSinghTakhi <class10a4974@gmail.com>
2025-06-12 13:15:40 +0530

    base done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import Header from './components/header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './index.css';
+import NoPage from './pages/Nopage/NoPage.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <button className="btn btn-primary m-4">
-        Bootstrap Button
-      </button>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NoPage/>} />
+        {/* Add more routes here */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
